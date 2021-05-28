d52e0c47c3b47010b79116e38df894cb5da761e3
Alexis Tercero <alexisdealmeyda@gmail.com>
2021-05-28 18:33:18 -0300

    Add custom hook useMoneda

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,9 +1,26 @@
-import React from 'react'
-import styled from "@emotion/styled"
+import React from 'react';
+import styled from "@emotion/styled";
+import useMoneda from "../hooks/useMoneda"
+
 
 const Formulario = () => {
-    return (
+    const MONEDAS = [
+        { codigo: "USD", nombre: "Dolar Estadounidense" },
+        { codigo: "ARS", nombre: "Peso Argento" },
+        { codigo: "MXN", nombre: "Peso Mexicano" },
+        { codigo: "EUR", nombre: "Euro" },
+        { codigo: "GBP", nombre: "Libra Esterlina" },
+    ]
+
+
+    //Utlizar useMoneda
+    const [ moneda, SelectMonedas ]= useMoneda("Elige tu moneda", "", MONEDAS);
+
+
+    return (  
         <form>
+            <SelectMonedas />
+
             <Boton 
                 type="submit"
                 value="Calcular"
