c530e538ba9a38bb36c37c5a9fcd7ab80e5d499d
Alexis Tercero <alexisdealmeyda@gmail.com>
2021-05-31 18:51:47 -0300

    Add useMoneda useCriptomoneda API

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from "@emotion/styled";
 import useMoneda from "../hooks/useMoneda"
+import useCriptomoneda from "../hooks/useCriptomoneda"
+import axios from "axios";
 
 
 const Formulario = () => {
+    //state del listado criptomonedas
+    const [ listacripto, guardarCriptomonedas ] = useState([]);
+
     const MONEDAS = [
         { codigo: "USD", nombre: "Dolar Estadounidense" },
         { codigo: "ARS", nombre: "Peso Argento" },
@@ -15,11 +20,23 @@ const Formulario = () => {
 
     //Utlizar useMoneda
     const [ moneda, SelectMonedas ]= useMoneda("Elige tu moneda", "", MONEDAS);
+   //Utilizar useCriptomoneda
+    const [ criptomoneda, SelectCripto ] = useCriptomoneda("Elige tu Criptomoneda","", listacripto);
+    //Ejecutar llamado a la API
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD"
 
+            const resultado = await axios.get(url);
 
+            guardarCriptomonedas(resultado.data.Data)            
+        }
+        consultarAPI();
+    }, []);
     return (  
         <form>
             <SelectMonedas />
+            <SelectCripto />
 
             <Boton 
                 type="submit"
